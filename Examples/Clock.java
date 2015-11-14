8665b3548e194686b6f82353e4ecfba4f20551a5
aapman55 <zhi-li_liu@live.nl>
2015-11-14 20:58:25 +0100

    Used new implemented Location function in Text

--- Examples/Clock.java
+++ Examples/Clock.java
@@ -1,9 +1,12 @@
 import org.aapframework.Utils.getTime;
 import org.aapframework.logger.Logger;
 import org.aapframework.logger.Logger.Severity;
+import org.aapframework.lwjgl.formObjects.Location;
 import org.aapframework.lwjgl.formObjects.Text;
 import org.aapframework.lwjgl.shapes.StdShapes;
 import org.aapframework.lwjgl.window.Window;
+import org.newdawn.slick.Color;
+
 import static org.lwjgl.opengl.GL11.*;
 
 public class Clock {
@@ -12,26 +15,34 @@ public class Clock {
 	public boolean start(){
 		// Create a window of size 1000px by 1000px with name 'Clock'
 		Window window = new Window(1000,1000,"Clock");
+		
 		// Tell openGL to show a 2D perspective
 		window.drawOn2D();
+		
 		// Do not show info messages
 		log.setReportingLevel(Severity.DEBUG);
+		
 		// Initialise string for timestamp
 		String currentTimeStamp;
+		
 		// Initialise hours, minutes and seconds
 		int hour = 0;
 		int minute = 0;
 		int second = 0;
+		
 		// Total seconds in half a day
 		int totalSecondsHalfDay = 60*60*12;
+		
 		// initialise current total seconds
 		int currentTotalSeconds = 0;
+		
 		// Initialise font
 		Text CONSOLAS = new Text("./res/Consolas.ttf");
 		
 		// Main drawing loop
 		while(!window.isCloseRequested()){
 			window.clearScreen();
+			
 			// Update currentTimestamp
 			currentTimeStamp = getTime.getCurrentTimeStamp("hh:mm:ss");
 			log.debug(currentTimeStamp);
@@ -49,22 +60,23 @@ public class Clock {
 			// Save transformation state
 			glPushMatrix();
 			glTranslated(window.getWindowWidth()/2, window.getWindowHeight()/2, 0);
+			
 			// Draw the outside
 			StdShapes.drawLineCircle(window.getWindowHeight()/2, 100, 5);
 			
 			// Draw the digital clock
-			CONSOLAS.draw((float)-CONSOLAS.getWidth(50, currentTimeStamp)/2f, window.getWindowHeight()/4f, 50f, currentTimeStamp);
+			CONSOLAS.draw(0, window.getWindowHeight()/3, 60, getTime.getCurrentTimeStamp("HH:mm:ss"), Color.white, Location.MID_CENTER);
 			
 			// Draw the numbers
-			CONSOLAS.draw((float)-CONSOLAS.getWidth(60, "12")/2f, -window.getWindowHeight()/2, 60, "12");
-			CONSOLAS.draw((float)window.getWindowWidth()/2f-(float)CONSOLAS.getWidth(60, "3"), (float)-CONSOLAS.getHeight(60)/2, 60, "3");
-			CONSOLAS.draw((float)-CONSOLAS.getWidth(60, "6")/2f, (float)(window.getWindowHeight()/2f-CONSOLAS.getHeight(60)), 60, "6");
-			CONSOLAS.draw((float)-window.getWindowWidth()/2f, (float)-CONSOLAS.getHeight(60)/2, 60, "9");
+			CONSOLAS.draw(window.getWindowWidth()/2, 0, 60, "3", Color.white, Location.MID_RIGHT);
+			CONSOLAS.draw(0, window.getWindowHeight()/2, 60, "6", Color.white, Location.BOT_CENTER);
+			CONSOLAS.draw(-window.getWindowWidth()/2, 0, 60, "9", Color.white, Location.MID_LEFT);
+			CONSOLAS.draw(0, -window.getWindowHeight()/2, 60, "12", Color.white, Location.TOP_CENTER);
 			
 			// Draw the hour pointer
 			glPushMatrix();
 			glRotated(currentTotalSeconds/(totalSecondsHalfDay*1.0)*360, 0, 0, 1);
-			glLineWidth(4);
+			glLineWidth(6);
 			glBegin(GL_LINES);
 				glVertex2d(0, 0);
 				glVertex2d(0, - window.getWindowWidth()/4);
