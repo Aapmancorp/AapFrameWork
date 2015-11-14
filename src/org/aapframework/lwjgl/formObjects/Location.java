d656303602c8649b30cc6097210ad31cf6fd71bb
aapman55 <zhi-li_liu@live.nl>
2015-11-14 20:59:29 +0100

    updated javadoc

--- src/org/aapframework/lwjgl/formObjects/Location.java
+++ src/org/aapframework/lwjgl/formObjects/Location.java
@@ -1,6 +1,6 @@
 package org.aapframework.lwjgl.formObjects;
 
-/** location of the string which is used as reference to place the string. */
+/** location on the object which is used as reference to place the object. */
 public enum Location{
 	TOP_LEFT, TOP_CENTER, TOP_RIGHT, MID_LEFT, MID_CENTER, MID_RIGHT, BOT_LEFT, BOT_CENTER, BOT_RIGHT;
 }
